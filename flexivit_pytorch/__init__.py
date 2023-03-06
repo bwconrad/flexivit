2cde10946c34efd24b5ff0fae26e4b6184ec945f
bwconrad <benwconrad@gmail.com>
2023-03-06 14:59:56 -0400

    Add models to imports

--- flexivit_pytorch/__init__.py
+++ flexivit_pytorch/__init__.py
@@ -1,3 +1,6 @@
+from flexivit_pytorch.flexivit import (FlexiVisionTransformer, flexivit_base,
+                                       flexivit_huge, flexivit_large,
+                                       flexivit_small, flexivit_tiny)
 from flexivit_pytorch.patch_embed import (FlexiPatchEmbed,
                                           interpolate_resize_patch_embed,
                                           pi_resize_patch_embed)
